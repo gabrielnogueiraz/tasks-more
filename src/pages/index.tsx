0a6aed6a19532cfa65ab069adc51df2f090d4a89
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-09-09 14:13:02 -0300

    removido o uso de dados estáticos na página inicial e integrado o componente StatsCounter para exibir contadores de posts e comentários

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
-import { GetStaticProps } from "next";
 import styles from "../../styles/Home.module.css";
 import heroImg from "../../public/assets/hero.png";
 import Image from "next/image";
+import { StatsCounter } from "../components/StatsCounter";
 
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../services/firebaseConnection";
-
-interface HomeProps {
-  posts: number;
-  comments: number;
-}
-
-export default function Home({ posts, comments }: HomeProps) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -28,31 +20,8 @@ export default function Home({ posts, comments }: HomeProps) {
           seus estudos e tarefas!
         </h1>
 
-        <div className={styles.infoContent}>
-          <section className={styles.box}>
-            <span>+{posts} posts</span>
-          </section>
-          <section className={styles.box}>
-            <span>+{comments} comentários</span>
-          </section>
-        </div>
+        <StatsCounter className={styles.infoContent} />
       </main>
     </div>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const commentRef = collection(db, "comments");
-  const postRef = collection(db, "tarefas");
-
-  const commentSnapshot = await getDocs(commentRef);
-  const postSnapshot = await getDocs(postRef);
-
-  return {
-    props: {
-      posts: postSnapshot.size || 0,
-      comments: commentSnapshot.size || 0,
-    },
-    revalidate: 60,
-  };
-};
